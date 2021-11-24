574883426bd5c43d54ba496c7c80cf97d263740d
Johannes Vedder <johannes.vedder@student.hpi.de>
2021-11-24 15:09:30 +0100

    wumpus geht jetzt endlich wirklich (hoffentlich)

--- src/wumpusworld/MyAgent/WumpusProbability.java
+++ src/wumpusworld/MyAgent/WumpusProbability.java
@@ -39,8 +39,8 @@ public class WumpusProbability {
             return null;
         }
         int size = BoardProbabilities.GetBoardSize();
-        for (int i=0; i < size; i++) {
-            for (int k=0; k < size; k++) {
+        for (int i = 0; i < size; i++) {
+            for (int k = 0; k < size; k++) {
                 if (BoardProbabilities.get_wumpusProbability(i, k) == 1) {
                     return new Point(i + 1, k + 1);
                 }
@@ -58,9 +58,9 @@ public class WumpusProbability {
         int size = BoardProbabilities.GetBoardSize();
 
         HashSet<Point> stenchFields = new HashSet<>();
-        for (int x=0; x < size; x++) {
-            for (int y=0; y < size; y++) {
-                if (_world.hasStench(x+1, y+1)) {
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                if (_world.hasStench(x + 1, y + 1)) {
                     stenchFields.add(new Point(x, y));
                 }
             }
@@ -116,9 +116,11 @@ public class WumpusProbability {
         List<Point> wumpusPossibilities = new ArrayList<>();
         for (Point visitedNeighbor : getVisitedNeighbors(point, _world)) {
             if (_world.hasStench(visitedNeighbor.x + 1, visitedNeighbor.y + 1)) {
-                for (Point neighbor : getNeighbors(visitedNeighbor, _world )) {
+                for (Point neighbor : getNeighbors(visitedNeighbor, _world)) {
                     if (!_world.isVisited(neighbor.x + 1, neighbor.y + 1)) {
                         if (wumpusPossibilities.contains(neighbor) && neighbor.x == point.x && neighbor.y == point.y) {
+                            return true;
+                        } else if (checkVisitedNeighborsWithoutStench(neighbor)) {
                             return true;
                         } else {
                             wumpusPossibilities.add(neighbor);
@@ -128,6 +130,15 @@ public class WumpusProbability {
                 /*if (wumpusPossibilities.contains(point) && wumpusPossibilities.do) {
                     return true;
                 }*/
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkVisitedNeighborsWithoutStench(Point neighbor) {
+        for (Point vN : getVisitedNeighbors(neighbor, _world)) {
+            if (!_world.hasStench(vN.x + 1, vN.y + 1)) {
+                return true;
             }
         }
         return false;
